48d9d3f05130e47981b9421ccc7ff81ef32d4a0f
amirrezahaqi <amirrezahaqi@gmail.com>
2024-04-22 21:09:48 +0330

    create search bar

--- lib/screens/mainscreen/home_screen.dart
+++ lib/screens/mainscreen/home_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:shopnew/screens/product_list_screen.dart';
 
-import '../../component/text_style.dart';
 import '../../res/colors.dart';
+import '../../widgets/searchbar_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -11,12 +10,8 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: AppColors.primaryColor,
-      child: Center(
-        child: ElevatedButton(
-          onPressed: () => Navigator.of(context).push(MaterialPageRoute(
-              builder: (context) => const ProductListScreen())),
-          child: const Text("مشاهده محصولات", style: LightAppTextStyle.title),
-        ),
+      child: const Center(
+        child: SearchBarWidget(),
       ),
     );
   }
---
+++ lib/widgets/searchbar_widget.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../component/text_style.dart';
+import '../gen/assets.gen.dart';
+import '../res/colors.dart';
+import '../res/dimens.dart';
+import '../res/strings.dart';
+
+class SearchBarWidget extends StatelessWidget {
+  const SearchBarWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(AppDimens.large),
+      child: Container(
+        height: 52,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: AppColors.searchBar,
+          borderRadius: BorderRadius.circular(50),
+          boxShadow: const [
+            BoxShadow(
+              blurRadius: 3,
+              color: AppColors.shadow,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SvgPicture.asset(Assets.svg.search),
+            const Text(
+              AppStrings.searchProduct,
+              style: LightAppTextStyle.hint,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(AppDimens.medium),
+              child: Image.asset(Assets.png.mainLogo.path),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
