bfd2ba64c4e28059da45e213843781c24ea98a2f
amirrezahaqi <amirrezahaqi@gmail.com>
2024-06-30 20:46:09 +0330

    add product repo

---
+++ lib/data/repo/product_repo.dart
@@ -0,0 +1,31 @@
+import 'package:shopnew/data/model/product.dart';
+import 'package:shopnew/data/src/product_data_src.dart';
+
+abstract class IProductRepository {
+  Future<List<Product>> getAllByCategory(int id);
+  Future<List<Product>> getAllByBrand(int id);
+  Future<List<Product>> getStored(String routeParam);
+  Future<List<Product>> searchProducts(String searchKey);
+}
+
+class ProductRepository implements IProductRepository {
+  final IProductDataSrc _iProductDataSrc;
+
+  ProductRepository(this._iProductDataSrc);
+
+  @override
+  Future<List<Product>> getAllByBrand(int id) =>
+      _iProductDataSrc.getAllByBrand(id);
+
+  @override
+  Future<List<Product>> getAllByCategory(int id) =>
+      _iProductDataSrc.getAllByCategory(id);
+
+  @override
+  Future<List<Product>> getStored(String routeParam) =>
+      _iProductDataSrc.getStored(routeParam);
+
+  @override
+  Future<List<Product>> searchProducts(String searchKey) =>
+      _iProductDataSrc.searchProducts(searchKey);
+}
